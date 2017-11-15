9dd34a59d13771b3cff02911b0cd34456ab7fe6b
Brian Clozel <bclozel@pivotal.io>
2017-11-15 21:52:36 +0100

    Get token configuration property from config server
    The GitHub API is rate-limited, so we'd like to use a GitHub token to
    upgrade the number of requests we're allowed to send per hour.
    
    Since this token is sensitive data, we don't want to commit that as
    plaintext in our application configuration.
    
    With this commit, we use the existing config server to fetch our
    configuration. The token information is encrypted in the configuration
    repository and deciphered on the fly by the config server (which holds a
    cipher key).
    
    With this setup, we assume that our config server is located in a DMZ
    and that clients are authorized to see that data. Note that there are
    other types of setups where clients can decipher configuration data
    themselves or use advanced features like Vault.

--- github-service/src/main/java/com/example/githubservice/integration/GithubClient.java
+++ github-service/src/main/java/com/example/githubservice/integration/GithubClient.java
@@ -3,6 +3,9 @@ package com.example.githubservice.integration;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +15,8 @@ public class GithubClient {
 
 	private final RestTemplate restTemplate;
 
+	private final Logger logger = LoggerFactory.getLogger(GithubClient.class);
+
 	public GithubClient(RestTemplate restTemplate) {
 		this.restTemplate = restTemplate;
 	}
@@ -20,12 +25,17 @@ public class GithubClient {
 	public List<GithubEvent> getRecentEvents(String username) {
 		ResponseEntity<GithubEvent[]> response = this.restTemplate
 				.getForEntity("https://api.github.com/users/{username}/events", GithubEvent[].class, username);
+
+		logger.info("X-RateLimit-Remaining: " + response.getHeaders().get("X-RateLimit-Remaining"));
 		return Arrays.asList(response.getBody());
 	}
 
 	public GithubUser getUser(String username) {
-		return this.restTemplate
-				.getForObject("https://api.github.com/users/{username}", GithubUser.class, username);
+		ResponseEntity<GithubUser> response = this.restTemplate
+				.getForEntity("https://api.github.com/users/{username}", GithubUser.class, username);
+
+		logger.info("X-RateLimit-Remaining: " + response.getHeaders().get("X-RateLimit-Remaining"));
+		return response.getBody();
 	}
 
 }
